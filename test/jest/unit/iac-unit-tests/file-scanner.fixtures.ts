01f1b6bb363e9d00a2ef718e7cfcd94a2214ed2d
Teodora Sandu <teodora.sandu@snyk.io>
2021-12-16 17:46:16 +0000

    replaced wasm with real ones - need to generate simpler ones

--- test/jest/unit/iac-unit-tests/file-scanner.fixtures.ts
+++ test/jest/unit/iac-unit-tests/file-scanner.fixtures.ts
@@ -8,8 +8,6 @@ import { SEVERITY } from '../../../../src/lib/snyk-test/common';
 
 export const expectedViolatedPoliciesForK8s: Array<PolicyMetadata> = [
   {
-    description: '',
-    id: '1',
     impact:
       'Compromised container could potentially modify the underlying host’s kernel by loading unauthorized modules (i.e. drivers).',
     issue: 'Container is running in privileged mode',
@@ -20,32 +18,42 @@ export const expectedViolatedPoliciesForK8s: Array<PolicyMetadata> = [
       'https://kubernetes.io/docs/concepts/policy/pod-security-policy/#privileged',
       'https://kubernetes.io/blog/2016/08/security-best-practices-kubernetes-deployment/',
     ],
+    remediation: {
+      kubernetes:
+        'Remove `securityContext.privileged` attribute, or set value to `false`',
+      terraform:
+        'Remove `security_context.privileged` attribute, or set value to `false`',
+    },
     resolve:
       'Remove `securityContext.privileged` attribute, or set value to `false`',
     severity: 'high' as SEVERITY,
     subType: 'Deployment',
-    title: 'Container is running in privileged mode',
-    type: 'k8s',
+    title: 'Privileged container',
   },
 ];
 
 export const expectedViolatedPoliciesForTerraform: Array<PolicyMetadata> = [
   {
-    description:
-      '## Overview\nUsing Terraform, the `aws_security_group` resource is used to restrict networking to and from different resources.\nWhen the ingress "cidr_blocks" is set to ["0.0.0.0/0"] or ["::/0"] potentially meaning everyone can access your resource.\n\n## Remediation\nThe aws_security_group ingress.cidr_block property should be populated with a specific IP range or address.\n\n## References\nad\n\n',
     id: '101',
     impact: 'That potentially everyone can access your resource',
     issue:
       'That inbound traffic is allowed to a resource from any source instead of a restricted range',
     msg: 'input.resource.aws_security_group[allow_ssh].ingress[0]',
     publicId: 'SNYK-CC-TF-1',
-    references: [],
+    references: [
+      'https://docs.aws.amazon.com/vpc/latest/userguide/VPC_SecurityGroups.html',
+    ],
+    remediation: {
+      cloudformation:
+        'Set `Properties.SecurityGroupIngress.CidrIp` attribute with a more restrictive IP, for example `192.16.0.0/24`',
+      terraform:
+        'Set `cidr_block` attribute with a more restrictive IP, for example `192.16.0.0/24`',
+    },
     resolve:
-      'Updating the `cidr_block` attribute with a more restrictive IP range or a specific IP address to ensure traffic can only come from known sources. ',
+      'Set `cidr_block` attribute with a more restrictive IP, for example `192.16.0.0/24`',
     severity: 'medium' as SEVERITY,
-    subType: 'Security Group',
+    subType: 'VPC',
     title: 'Security Group allows open ingress',
-    type: 'terraform',
   },
 ];
 
--- test/jest/unit/iac-unit-tests/file-scanner.spec.ts
+++ test/jest/unit/iac-unit-tests/file-scanner.spec.ts
@@ -25,8 +25,14 @@ describe('scanFiles', () => {
     parsedTerraformFileStub,
     parsedArmFileStub,
   ];
+  let spy;
+
+  beforeEach(() => {
+    spy = jest.spyOn(localCacheModule, 'getLocalCachePath');
+  });
 
   afterEach(() => {
+    spy.mockReset();
     clearPolicyEngineCache();
   });
 
@@ -41,52 +47,58 @@ describe('scanFiles', () => {
         path.join('../../../smoke', LOCAL_POLICY_ENGINE_DIR),
       );
 
-      const spy = jest
-        .spyOn(localCacheModule, 'getLocalCachePath')
-        .mockImplementation((engineType: EngineType) => {
-          switch (engineType) {
-            case EngineType.Kubernetes:
-              return [
-                `${policyEngineCoreDataPath}/k8s_policy.wasm`,
-                `${policyEngineMetaDataPath}/k8s_data.json`,
-              ];
-            case EngineType.Terraform:
-              return [
-                `${policyEngineCoreDataPath}/tf_policy.wasm`,
-                `${policyEngineMetaDataPath}/tf_data.json`,
-              ];
-            case EngineType.CloudFormation:
-              return [
-                `${policyEngineCoreDataPath}/cloudformation_policy.wasm`,
-                `${policyEngineMetaDataPath}/cloudformation_data.json`,
-              ];
-            case EngineType.ARM:
-              return [
-                `${policyEngineCoreDataPath}/arm_policy.wasm`,
-                `${policyEngineMetaDataPath}/arm_data.json`,
-              ];
-            default:
-              return [];
-          }
-        });
+      spy.mockImplementation((engineType: EngineType) => {
+        switch (engineType) {
+          case EngineType.Kubernetes:
+            return [
+              `${policyEngineCoreDataPath}/k8s_policy.wasm`,
+              `${policyEngineMetaDataPath}/k8s_data.json`,
+            ];
+          case EngineType.Terraform:
+            return [
+              `${policyEngineCoreDataPath}/tf_policy.wasm`,
+              `${policyEngineMetaDataPath}/tf_data.json`,
+            ];
+          case EngineType.CloudFormation:
+            return [
+              `${policyEngineCoreDataPath}/cloudformation_policy.wasm`,
+              `${policyEngineMetaDataPath}/cloudformation_data.json`,
+            ];
+          case EngineType.ARM:
+            return [
+              `${policyEngineCoreDataPath}/arm_policy.wasm`,
+              `${policyEngineMetaDataPath}/arm_data.json`,
+            ];
+          default:
+            return [];
+        }
+      });
 
       const scanResults = await scanFiles(parsedFiles);
       expect(scanResults[0].violatedPolicies).toEqual(
-        expectedViolatedPoliciesForK8s,
+        expect.arrayContaining([
+          expect.objectContaining(expectedViolatedPoliciesForK8s[0]),
+        ]),
       );
       expect(scanResults[1].violatedPolicies).toEqual(
-        expectedViolatedPoliciesForTerraform,
+        expect.arrayContaining([
+          expect.objectContaining(expectedViolatedPoliciesForTerraform[0]),
+        ]),
       );
       expect(scanResults[2].violatedPolicies).toEqual(
-        expectedViolatedPoliciesForArm,
+        expect.arrayContaining([
+          expect.objectContaining(expectedViolatedPoliciesForArm[0]),
+        ]),
       );
-      spy.mockReset();
     });
     // TODO: Extract policy engine & the cache mechanism, test them separately.
   });
 
   describe('missing policy engine wasm files', () => {
     it('throws an error', async () => {
+      spy.mockImplementation(() => {
+        return ['invalid.wasm', 'invalid.json'];
+      });
       await expect(scanFiles(parsedFiles)).rejects.toThrow();
     });
   });
