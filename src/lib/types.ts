yuvicode/snyk

feat: IaC Snapshot PoC

src/lib/types.ts
@@ -89,6 +89,7 @@ export interface Options {
   'fail-fast'?: boolean;
   tags?: string;
   'target-reference'?: string;
+  snapshot?: boolean;
 }
 
 // TODO(kyegupov): catch accessing ['undefined-properties'] via noImplicitAny

snyk-nebula-test/cli

src/lib/types.ts
@@ -90,6 +90,7 @@ export interface Options {
   tags?: string;
   'target-reference'?: string;
   'exclude-base-image-vulns'?: boolean;
+  report?: boolean;
 }
 
 // TODO(kyegupov): catch accessing ['undefined-properties'] via noImplicitAny